ef8f6f2c0950100f7f2f1b8c53224f52fe938f45
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2026-03-26 10:21:40 -0400

    Fix path for inferno_fs_patcher movement

--- ios-setup.sh
+++ ios-setup.sh
@@ -89,7 +89,7 @@ fi
 cd InfernoFSPatcher
 cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON
 cmake --build build
-mv build/inferno_fs_patcher .
+mv build/inferno_fs_patcher ..
 rm -rf InfernoFSPatcher
 
 # Download SHSH blob
