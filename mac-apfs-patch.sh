27d8f7c08532fbc0a36aae7d6315b62419183d9a
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2026-03-26 10:05:29 -0400

    Refactor mac-apfs-patch.sh for inferno_fs_patcher
    Removed cmake installation steps and added check for inferno_fs_patcher.

--- mac-apfs-patch.sh
+++ mac-apfs-patch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # NOTE: This script must be run on macOS.
+# git and cmake are required
 
 set -e
 
@@ -9,47 +10,22 @@ set -e
     exit
 }
 
-[ ! -f Inferno/build/root ] || cd Inferno/build
-
 [ -f root ] || {
   echo "APFS not found. Exiting..."
   exit 1
 }
 
-if ! xcode-select -p &>/dev/null; then
-    xcode-select --install 2>/dev/null || true
-
-    # Wait until installation completes
-    while ! xcode-select -p &>/dev/null; do
-        sleep 5
-    done
-fi
-
+[ -f inferno_fs_patcher ] || {
+  echo "inferno_fs_patcher not found. Exiting..."
+  exit 1
+}
 
 # Mount the APFS with read/write access
 hdiutil attach -imagekey diskimage-class=CRawDiskImage -blocksize 4096 -noverify -noautofsck root
 sudo diskutil enableownership /Volumes/System
 sudo mount -urw /Volumes/System
-cd
 
-# Install cmake
-if ! command -v cmake >/dev/null; then
-  curl -sLO https://github.com/Kitware/CMake/releases/download/v4.3.0/cmake-4.3.0-macos-universal.tar.gz
-  tar -xzf cmake-4.3.0-macos-universal.tar.gz
-  rm cmake-4.3.0-macos-universal.tar.gz
-  sudo mv cmake-4.3.0-macos-universal /usr/local/cmake-4.3.0
-  echo 'export PATH="/usr/local/cmake-4.3.0/CMake.app/Contents/bin:$PATH"' >> ~/.zshrc
-  export PATH="/usr/local/cmake-4.3.0/CMake.app/Contents/bin:$PATH"
-fi
-
-# Patch the Dyld Shared Cache
-[ -d InfernoFSPatcher ] || git clone https://git.chefkiss.dev/AppleHax/InfernoFSPatcher
-cd InfernoFSPatcher
-cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON
-cmake --build build
-sudo build/inferno_fs_patcher /Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
-cd ..
-rm -rf InfernoFSPatcher
+sudo ./inferno_fs_patcher /Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 
 # Disable the Problematic Launch Services
 sudo cp /Volumes/System/System/Library/xpc/launchd.plist /Volumes/System/System/Library/xpc/launchd.plist.orig
